3bb892e69091e1353d591ce5059cd0ac4a9874d3
goga133 <asromanyuk@edu.hse.ru>
2020-12-16 17:06:37 +0300

    Добавлена авторизация с возвращением токенов.

--- Server/src/main/kotlin/com/goga133/hsecoffee/controllers/AuthController.kt
+++ Server/src/main/kotlin/com/goga133/hsecoffee/controllers/AuthController.kt
@@ -1,15 +1,19 @@
-
 package com.goga133.hsecoffee.controllers
 
+import com.fasterxml.jackson.databind.JsonSerializer
+import com.fasterxml.jackson.databind.ser.std.JsonValueSerializer
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.goga133.hsecoffee.service.EmailService
+import com.goga133.hsecoffee.service.JwtService
+import com.goga133.hsecoffee.service.RefreshTokenService
+import com.goga133.hsecoffee.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.json.JsonParser
+import org.springframework.boot.json.JsonParserFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.*
 
 @Controller
 class AuthController() {
@@ -17,19 +21,53 @@ class AuthController() {
     @Autowired
     private val emailService: EmailService? = null
 
+    @Autowired
+    private val userService: UserService? = null
+
+    @Autowired
+    private val jwtService: JwtService? = null
+
+    @Autowired
+    private val refreshTokenService: RefreshTokenService? = null
+
     @RequestMapping(value = ["/api/confirm"], method = [RequestMethod.POST])
     @ResponseStatus(HttpStatus.OK)
     fun confirm(
-        @RequestParam(value = "email") email: String,
-        @RequestParam(value = "code") code: Int,
+        @RequestBody body: String
     ): ResponseEntity<String> {
-        if(emailService?.isValid(email, code) == true){
-            // TODO: Вернём JWT + RT
+        val json = JsonParserFactory.getJsonParser().parseMap(body)
+
+        val email: String
+        val code: Int
+        val fingerPrint: String
+        try {
+            email = json["email"] as String
+            code = json["code"] as Int
+            fingerPrint = json["fingerPrint"] as String
+        } catch (e: ClassCastException) {
+            return ResponseEntity.badRequest().body(e.message)
+        }
+
+        if (emailService?.isValid(email, code) == true) {
+            val user = userService?.getUserByEmailOrCreate(email) ?:
+                return ResponseEntity.badRequest().body("Server Error")
+
+            val accessToken = jwtService?.createAccessToken(user)
+            val refreshToken = refreshTokenService?.createByUser(user, fingerPrint)?.uuid
+
+            val response = jacksonObjectMapper().
+            writeValueAsString(mapOf("accessToken" to accessToken, "refreshToken" to refreshToken))
+
+            return ResponseEntity.ok(response)
         }
 
         return ResponseEntity.badRequest().body("Incorrect code or email.")
     }
 
-
+    @RequestMapping(value = ["/api/refresh"], method = [RequestMethod.POST])
+    @ResponseStatus(HttpStatus.OK)
+    fun refresh(){
+        TODO("Не реализовано.")
+    }
 }
 
