916afa7350b8995a462d828fca193d64074463bb
goga133 <asromanyuk@edu.hse.ru>
2021-01-11 13:08:46 +0300

    Добавлены java-doc комментарии.

--- Server/src/main/kotlin/com/goga133/hsecoffee/service/EmailService.kt
+++ Server/src/main/kotlin/com/goga133/hsecoffee/service/EmailService.kt
@@ -11,6 +11,15 @@ import org.springframework.transaction.annotation.Transactional
 import java.time.Instant
 import java.util.*
 
+/**
+ * Сервис для работы с Email, в частности с протоколом SMTP для отправки EMAIL-сообщений.
+ *
+ * @param text - Текст письма.
+ * @param subject - Тема письма.
+ * @param from - Email-адресс отправителя.
+ * @param lifeTime - Время действия кода в милисекундах.
+ * @param domains - Разрешенные домены для почт получателя. Разделяются ';'.
+ */
 @Service
 class EmailService(
     private val javaMailSender: JavaMailSender,
@@ -21,38 +30,49 @@ class EmailService(
     @Value("\${mail.domains}") private val domains: String
 ) {
 
+    companion object {
+        /**
+         * Минимальная длина логика у EMAIL адреса. Логином считается первая часть, разделённая @.
+         * Например: abcd@edu.hse.ru => логин abcd.
+         */
+        const val MIN_LENGTH_LOGIN = 3
+    }
+
     @Autowired
     private val confirmationCodeRepository: ConfirmationCodeRepository? = null
 
-    private fun sendCode(receiver: String, code: Int) {
-        val message = SimpleMailMessage()
-
-        message.setSubject(subject)
-        message.setText(text.replace("{code}", code.toString()))
-        message.setFrom(from)
-        message.setTo(receiver)
-
-        javaMailSender.send(message)
-    }
-
+    /**
+     * Проверка на валидность кода относительно Email-адреса.
+     * @param receiver - email-адрес.
+     * @param code - код, отправленный на email-адрес.
+     * @return true - если код верный, false - если код неверный или произошла ошибка.
+     */
     fun isValidCode(receiver: String, code: Int): Boolean {
         val confirmationCode = confirmationCodeRepository?.findByEmail(receiver) ?: return false
 
         if (confirmationCode.code == code && Instant.now().minusMillis(confirmationCode.createdDate.time)
                 .toEpochMilli() <= lifeTime
-        )
+        ) {
             return true
+        }
+
 
         return false
     }
 
+    /**
+     * Проверка на валидность Email-адреса.
+     * Валидным Email-адресом считается такой, который содержит больше @code {MIN_LENGTH_LOGIN} символов.
+     * @see MIN_LENGTH_LOGIN
+     * @param email - email-адрес, проверяемый на корректность.
+     * @return true - если адрес корректный, иначе - false.
+     */
+    fun isValidMail(email: String?): Boolean {
+        if (email.isNullOrEmpty())
+            return false
 
-    fun isValidMail(email : String?) : Boolean{
-        if(email == null)
-            return false;
-
-        for (domain in domains.split(";")){
-            if(email.endsWith(domain)){
+        domains.split(";").forEach { domain ->
+            if (email.endsWith(domain) && email.length - domain.length > MIN_LENGTH_LOGIN) {
                 return true
             }
         }
@@ -60,6 +80,11 @@ class EmailService(
         return false
     }
 
+    /**
+     * Метод для генерации, создания и отправки кода на Email-адрес.
+     * @param receiver - email-адрес, на который нужно отправить код.
+     * @return True - если отправка успешна, False - иначе.
+     */
     @Transactional
     fun trySendCode(receiver: String): Boolean {
         try {
@@ -94,7 +119,29 @@ class EmailService(
         return false
     }
 
-    fun setLifeTime(lifeTime: Int){
+    /**
+     * Ручное выставление времени. Используется для Unit-тестов.
+     * @param lifeTime - время действия кода в милисекундах.
+     */
+    fun setLifeTime(lifeTime: Int) {
         this.lifeTime = lifeTime
+    }
+
+    /**
+     * Метод для отправки целочисленного кода на Email-адрес.
+     * @param receiver - email - получатель кода.
+     * @param code - целочисленный код.
+     */
+    private fun sendCode(receiver: String, code: Int) {
+        val message = SimpleMailMessage()
+
+        message.apply {
+            subject?.let { setSubject(it) }
+            text?.let { setText(it.replace("{code}", code.toString())) }
+            from?.let { setFrom(it) }
+            setTo(receiver)
+        }
+
+        javaMailSender.send(message)
     }
 }
