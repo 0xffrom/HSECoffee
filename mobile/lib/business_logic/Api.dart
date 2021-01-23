fe70b0a213eb2ecf192aff00001d36306f3bbcf6
goga133 <asromanyuk@edu.hse.ru>
2021-01-24 01:20:44 +0300

    http put user

--- mobile/lib/business_logic/Api.dart
+++ mobile/lib/business_logic/Api.dart
@@ -2,12 +2,18 @@ import 'dart:convert';
 
 import 'package:hse_coffee/business_logic/Auth.dart';
 import 'package:hse_coffee/business_logic/EventWrapper.dart';
-import 'package:hse_coffee/data/User.dart';
+import 'package:hse_coffee/data/user.dart';
 import 'package:http/http.dart' as http;
+
+import 'UserStorage.dart';
 
 class Api {
   // http://188.120.233.197
   static const String ip = "http://10.0.2.2:8081";
+
+  static const Map<String, String> _JSON_HEADERS = {
+    "content-type": "application/json"
+  };
 
   static Future<EventWrapper<bool>> sendCode(String email) async {
     print("/api/code?email=$email");
@@ -51,17 +57,44 @@ class Api {
         response.statusCode, null, "Связь с сервером не была установлена");
   }
 
+  static Future<EventWrapper<bool>> setUser(User user) async {
+    final accessToken = (await Auth.getData())["access"];
+    print("PUT: /api/user. accessToken = [$accessToken]");
+
+    var jsonEnc = json.encode(user.toJson());
+    final response = await http.put('$ip/api/user/settings/$accessToken', body: jsonEnc, headers: _JSON_HEADERS);
+
+    print("Код: ${response.statusCode}");
+
+    if (response.statusCode == 200) {
+      return EventWrapper(response.statusCode, true, "Удачно");
+    }
+
+    if ((response.statusCode == 403 || response.statusCode == 401) &&
+        (await _updateTokens()) == true) {
+      return setUser(user);
+    }
+
+    if (response.body != null)
+      return EventWrapper(response.statusCode, null, response.body);
+
+    return EventWrapper(
+        response.statusCode, null, "Связь с сервером не была установлена");
+
+  }
+
   static Future<EventWrapper<User>> getUser() async {
     final accessToken = (await Auth.getData())["access"];
-    print("/api/user. acessToken = [$accessToken]");
+    print("GET: /api/user. accessToken = [$accessToken]");
 
     final response = await http.get('$ip/api/user/settings/$accessToken');
 
     print("Код: ${response.statusCode}");
 
     if (response.statusCode == 200) {
-      return EventWrapper(response.statusCode,
-          User.fromJson(jsonDecode(response.body)), "Удачно");
+      var user =  User.fromJson(jsonDecode(response.body));
+
+      return EventWrapper(response.statusCode, user, "Удачно");
     }
 
     if ((response.statusCode == 403 || response.statusCode == 401) &&
