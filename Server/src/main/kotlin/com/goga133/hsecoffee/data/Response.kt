7fd0ef8b9c004f5d7b636d4a675884ca4b910637
goga133 <asromanyuk@edu.hse.ru>
2021-01-28 18:48:17 +0300

    deleted

--- Server/src/main/kotlin/com/goga133/hsecoffee/data/Response.kt
+++
@@ -1,8 +0,0 @@
-package com.goga133.hsecoffee.data
-
-import org.springframework.http.HttpStatus
-
-/**
- * Data class. Предназначается для сервисов, чтобы отдать контроллеру уже готовый ответ в случае ошибки.
- */
-data class Response(val httpStatus: HttpStatus, val message: String, val email: String?)
